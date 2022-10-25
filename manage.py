48275dbfd6007be28ac35cda5466cc231b0fdebf
aitorgude <106232308+aitorgude@users.noreply.github.com>
2022-10-25 13:21:24 +0200

    unpatch FIPS

--- manage.py
+++ manage.py
@@ -1,35 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import hashlib
-import importlib
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "katacoda.settings")
     try:
-        def _non_security_md5(*args, **kwargs):
-    kwargs['usedforsecurity'] = False
-    return hashlib.md5(*args, **kwargs)
-
-def monkey_patch_md5(modules_to_patch):
-    """Monkey-patch calls to MD5 that aren't used for security purposes.
-
-    Sets RHEL's custom flag `usedforsecurity` to False allowing MD5 in FIPS mode.
-    `modules_to_patch` must be an iterable of module names (strings).
-    Modules must use `import hashlib` and not `from hashlib import md5`.
-    """
-    # Manually load a module as a unique instance
-    # https://stackoverflow.com/questions/11170949/how-to-make-a-copy-of-a-python-module-at-runtime
-    HASHLIB_SPEC = importlib.util.find_spec('hashlib')
-    patched_hashlib = importlib.util.module_from_spec(HASHLIB_SPEC)
-    HASHLIB_SPEC.loader.exec_module(patched_hashlib)
-
-    patched_hashlib.md5 = _non_security_md5  # Monkey patch MD5
-
-    # Inject our patched_hashlib for all requested modules
-    for module_name in modules_to_patch:
-        module = importlib.import_module(module_name)
-        module.hashlib = patched_hashlib
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
